d73547722ad6f44c664eb7b069cc566600fc701c
Magnus Ulimoen <magnusu@met.no>
2022-02-02 16:29:12 +0100

    First prototype of dry deposition

--- src/common/readfield_nc.f90
+++ src/common/readfield_nc.f90
@@ -360,11 +360,13 @@ subroutine readfield_nc(istep, backward, itimei, ihr1, ihr2, &
 !..mean sea level pressure, not used in computations,
 !..(only for output to results file)
   if(imslp /= 0) then
-    if ( .NOT. met_params%mslpv == '') then
+    if (met_params%mslpv /= '') then
+      call nfcheckload(ncid, met_params%mslpv, start3d, count3d, pmsl2(:,:))
+    else if (met_params%psv /= '') then
+      call nfcheckload(ncid, met_params%psv, start3d, count3d, pmsl2(:,:))
+    else
       write(iulog,*) 'Mslp not found. Not important.'
       imslp=0
-    else
-      call nfcheckload(ncid, met_params%mslpv, start3d, count3d, pmsl2(:,:))
     end if
   end if
 
@@ -604,7 +606,7 @@ subroutine readfield_nc(istep, backward, itimei, ihr1, ihr2, &
     end do
   end if
 
-  return
+  call read_drydep()
 end subroutine readfield_nc
 
 !> Reads `units` attribute of the precipitation variable
@@ -988,4 +990,12 @@ subroutine nfcheckload3d(ncid, varname, start, length, field, return_status)
     field = field*factor + offset
   end if
 end subroutine nfcheckload3d
+
+subroutine read_drydep()
+  use drydep, only: drydep_scheme, DRYDEP_SCHEME_EMEP
+
+  if (drydep_scheme == DRYDEP_SCHEME_EMEP) then
+    error stop "Reading of dry deposition is not implemented for netCDF"
+  endif
+end subroutine
 end module readfield_ncML
